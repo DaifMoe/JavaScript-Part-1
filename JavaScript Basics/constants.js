1b9c0d071c41faf3e8cbb52b716046a2f2707c42
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-19 22:20:02 -0400

    Practice with constants

---
+++ JavaScript Basics/constants.js
@@ -0,0 +1,6 @@
+// Variables are able to be re-declared as new values as they vary (hence variables)
+// Constants are used when we have a never changing value that will not be reassigned
+
+// Because we used const for interestRate it's value is always 0.3 and will never be able to be changed
+const interestRate = 0.3;
+console.log(interestRate);
