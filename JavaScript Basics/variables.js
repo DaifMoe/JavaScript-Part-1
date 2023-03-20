de398f586992dba34251962f70ecfb89e5387760
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-19 22:16:03 -0400

    Practice with variables

---
+++ JavaScript Basics/variables.js
@@ -0,0 +1,13 @@
+let namez = "Moe";
+console.log(namez);
+
+// Rules for naming variables
+// Rule 1: Name cannot be a reserved keyword
+// Rule 2: Names should be meaningful and have purpose
+// Rule 3: Names cannot start with a number (1name)
+// Rule 4: Cannot contain a space or hyphen (-)
+// Rule 5: Variable names should be in camel case notation
+// Rule 6: JavaScript is case sensitive so be careful
+
+let firstName = "Moe";
+let lastName = "Daif";
