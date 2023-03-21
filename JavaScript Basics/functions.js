6f4654b21187cb661c0ba4fc96b7166a3de86cf9
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-20 21:52:44 -0400

    Practicing Functions

---
+++ JavaScript Basics/functions.js
@@ -0,0 +1,9 @@
+// JavaScript function has a name function name and parameter function name(parameter)
+// To call function write it's name where it needs to be called and pass it arguments
+
+function greet(name, lastName) {
+  console.log("Hello " + name + " " + lastName);
+}
+
+greet("John", "Mikeson");
+greet("Mike", "Johnson");
