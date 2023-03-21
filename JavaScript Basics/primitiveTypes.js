b8a2dd223e14e092e316a6b26ff456518d2e9d56
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-20 21:23:01 -0400

    Practice with Primitive Types

---
+++ JavaScript Basics/primitiveTypes.js
@@ -0,0 +1,5 @@
+let namez = "Moe"; // This is a string literal (string)
+let age = 20; // This is a number literal (number)
+let isApproved = true; // This is a boolean literal (boolean)
+let firstName = undefined;
+let selectedColor = null; // We use this when we want to explicitly clear the value of a variable
