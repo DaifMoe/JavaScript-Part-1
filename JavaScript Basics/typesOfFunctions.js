5e4ed175e99c47202d6af53f82bbae6c5295f9b4
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-20 22:01:35 -0400

    Types of Functions and Practicing Them

---
+++ JavaScript Basics/typesOfFunctions.js
@@ -0,0 +1,11 @@
+// Performing a task
+function greet(name, lastName) {
+  console.log("Hello " + name + " " + lastName);
+}
+
+// Example of function that is calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
