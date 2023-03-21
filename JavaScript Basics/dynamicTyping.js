b66b63053fd3f77c40c268a9bf5efe8cb6decd47
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-20 21:30:53 -0400

    Dynamic JS Typing (Static vs Dynamic Langs)

---
+++ JavaScript Basics/dynamicTyping.js
@@ -0,0 +1,10 @@
+// JavaScript is a Dynamically Typed Language meaning that
+// if we assign a value to a variable it can be reassigned later
+
+// Java is a static language so it expects variables to be declared
+// before they can be assigned values
+let namez = "Moe";
+let age = 20;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
