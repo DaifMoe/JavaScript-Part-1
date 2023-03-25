d7c9ed41e1638e7e7f05d1ad38a5a80607a163bd
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-25 16:46:20 -0400

    Comparison Operators Practice

---
+++ JavaScript Basics/comparisonOperators.js
@@ -0,0 +1,11 @@
+let x = 1;
+
+// These four operators are relational operators
+console.log(x > 0); // true
+console.log(x >= 1); // true
+console.log(x < 1); // false
+console.log(x <= 1); // true
+
+// This is the Equality Operator
+console.log(x === 1); // true
+console.log(x !== 1); // false
