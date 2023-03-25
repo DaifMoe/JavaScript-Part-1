01ff4db263b0bc25be9f066c10dbf43598ee32f9
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-25 16:51:50 -0400

    Equality Operators Practice

---
+++ JavaScript Basics/equalityOperators.js
@@ -0,0 +1,8 @@
+// Strict Equality Operator (Ensures both values on the sides of the operator have the same type and value)
+console.log(1 === 1); // This would be true
+console.log("1" === 1); // This would be false
+
+// Loose Equality Operator (Ensures both are the same value but it doesn't care if they aren't the same type)
+console.log(1 == 1); // This would be true
+console.log("1" == 1); // This would be true
+console.log(true == 1); // This would be true
