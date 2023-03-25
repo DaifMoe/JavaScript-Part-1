7cdc033acc01d7b962cf4c55e9cf4aa79bd6fe0e
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-25 16:26:50 -0400

    Practicing with Arithmetic Operators

---
+++ JavaScript Basics/arithmeticOperators.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y); // Addition Operator
+// console.log(x - y); // Subtraction Operator
+// console.log(x * y); // Multiplication Operator
+// console.log(x / y); // Division Operator
+// console.log(x % y); // Remainder Division Operator
+// console.log(x ** y); // Exponentiation Operator
+
+// Increment Operator (++) (Depending on where the two plus signs are placed the behavior will be different)
+// For example if you do console.log(++x) this results in 11
+// but if you did console.log(x++) if would print 10 first then add 1 to the value of x making it 11
+console.log(x);
+
+// Decrement Operator (--) (Depending on where the two minus signs are placed the behavior will be different)
+// For example if you do console.log(--x) this results in 9
+// but if you did console.log(x--) if would print 10 first then add 1 to the value of x making it 9
