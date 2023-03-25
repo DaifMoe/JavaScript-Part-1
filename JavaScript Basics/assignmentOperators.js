be1f8053662d83dbd432250bd05767c6d46f5a1e
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-03-25 16:30:54 -0400

    Assignment Operators Practice

---
+++ JavaScript Basics/assignmentOperators.js
@@ -0,0 +1,13 @@
+let x = 10;
+
+x = x + 5;
+
+// the more efficient way to write the above statement is down below
+x += 5;
+
+x = x * 3;
+
+// the more efficient way to write the above statement is down below
+x *= 5;
+
+// Lastly you can apply this to any of the arithmetic operators from earlier!
