19707b4399854d2ee36bdbdd5d63eeedb9ad4a9c
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-02 17:11:11 -0400

    Infinite Loop Examples

---
+++ Control Flow/InfiniteLoop.js
@@ -0,0 +1,14 @@
+// Infinite Loop Example
+let i = 0;
+while (i < 5) {
+  console.log(i);
+}
+
+// while (true) {}
+
+// do {} while (true);
+
+// let x = 0;
+// do {} while (x < 5);
+
+// for (let i = 0; i < 10)
