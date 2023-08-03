c790efc0cd97dceabf95ea6b7bc26ff954b1fa82
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 14:10:45 -0400

    Understanding Constructor Properties

---
+++ Objects/ConstructorProperty.js
@@ -0,0 +1,5 @@
+new String(); // '', "", ``
+new Boolean(); // True, False
+new Number(); // 1, 2, 3
+
+// Every Object has a constructor property that referecnes the function used to create that object
