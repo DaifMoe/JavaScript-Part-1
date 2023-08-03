b0bee0bacbdb9ca18414eaced99ab789d34f5227
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 17:03:24 -0400

    Blog Post Object Ex Get Comfortable With Objects

---
+++ Objects/BlogPostObjectExercise.js
@@ -0,0 +1,21 @@
+// Create a blog psot object with the following
+// Title
+// Body
+// Author
+// Views
+// Comments should have author and body
+// isLive (true or false)
+
+let blogPost = {
+  title: "CodersBlog",
+  body: "JavaScript Ninja",
+  author: "The Smithsonian",
+  views: 500,
+  comments: [
+    { author: "Bread", body: "This is cool!" },
+    { author: "Brake", body: "Who's Brake?" },
+  ],
+  isLive: true,
+};
+
+console.log(post);
