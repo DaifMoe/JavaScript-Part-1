9f8afb58cf26822e90baaa528141bd3c43c3a2c7
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 14:16:54 -0400

    Functions Are Objects Example

---
+++ Objects/FunctionsAreObjects.js
@@ -0,0 +1,19 @@
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+// const Circle1 = new Function(
+//   "radius",
+//   `this.radius = radius;
+// this.draw = function () {
+//   console.log("draw");
+// }
+// `
+// );
+
+// const circle = new Circle(1);
+
+const another = new Circle(1);
