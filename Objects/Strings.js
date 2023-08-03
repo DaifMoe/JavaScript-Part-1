d1cc72697f9697f616575073aca3aee2883ea02e
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 15:24:42 -0400

    Strings and string manipulation examples

---
+++ Objects/Strings.js
@@ -0,0 +1,7 @@
+// String Primitive
+const message = "This is my first message";
+console.log(message.length);
+console.log(message.toUpperCase);
+
+// String Object
+const another = new String("hi");
