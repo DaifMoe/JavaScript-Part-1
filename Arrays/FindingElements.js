f54b86d66858bd356f6087e2ee62b0fc0cbc6e4b
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 22:27:33 -0400

    Finding Elements in an Array Examples

---
+++ Arrays/FindingElements.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 1, 4];
+// indexOf returns the index of the value in the array
+// if it is present and if not present it will return -1
+console.log(numbers.indexOf(1));
+console.log(numbers.lastIndexOf(1));
+
+console.log(numbers.includes(1));
