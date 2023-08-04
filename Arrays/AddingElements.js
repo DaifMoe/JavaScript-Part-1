213432607982fe5282332387041c3ae9bbd5233e
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 22:18:48 -0400

    Adding Elements To Arrays at Various Points

---
+++ Arrays/AddingElements.js
@@ -0,0 +1,13 @@
+// Adding Elements To An Array
+const numbers = [3, 4];
+
+// End
+numbers.push(5, 6);
+
+// Beginning
+numbers.unshift(1, 2);
+
+// Middle
+numbers.splice(2, 0, "a", "b");
+
+console.log(numbers);
