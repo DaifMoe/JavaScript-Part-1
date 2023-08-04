16777753e877d731ae2aff1f00547bb7acd23ac7
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-03 23:43:42 -0400

    Removing Elements From An Array

---
+++ Arrays/RemovingElements.js
@@ -0,0 +1,15 @@
+// Removing elements from an array
+const numbers = [1, 2, 3, 4];
+
+// End
+const last = numbers.pop();
+// console.log(numbers);
+// console.log(last);
+
+// Beginning
+const first = numbers.shift();
+// console.log(first);
+
+// Middle
+numbers.splice(2, 1);
+console.log(numbers);
