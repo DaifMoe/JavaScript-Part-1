555d983f29aa927126e2158f01eb9999d17f69af
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-05 17:03:25 -0400

    Get Max from an Array Example

---
+++ Arrays/GetMax.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 4];
+
+const max = getMax(numbers);
+
+console.log(max);
+
+//
+function getMax(array) {
+  if (array.length === 0) return undefined;
+
+  return array.reduce((a, b) => (a > b ? a : b));
+}
+
+// Basic Implementation
+// function getMax(array) {
+//   if (array.length === 0) return undefined;
+
+//   let max = array[0];
+
+//   for (let i = 1; i < array.legnth; i++) if (array[i] > max) max = array[i];
+//   return max;
+// }
